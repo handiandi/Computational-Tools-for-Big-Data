bb1b608df33126fe7bed81379fc73f523ec241e1
Soren Howe Gersager <syrelyre@gmail.com>
2015-09-08 20:33:13 +0200

    2-3 now uses a regex

--- Lesson 2/exercise-2-3.py
+++ Lesson 2/exercise-2-3.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import json
+import re
+
 # first we open the json-encoded pizza train file and load it into a list of dictionaries
 with open("pizza-train.json") as f:
-	json_string = f.read()
+	dicts = json.loads(f.read())
 
-dicts = json.loads(json_string)
+# we use a list comprehension to extract each request_text and split it using a regex generating a list of lists
+text_lists = [re.split("\W+",dictionary["request_text"]) for dictionary in dicts]
 
-# we use a list comprehension to extract each request_text and split it using str split
-# a regex could also be used using the re library
-text_lists = [dictionary["request_text"].split() for dictionary in dicts]
-# flatten and set it to only get distinct words, in our case we get 25083 words
+# flatten and set it to only get distinct words, in our case we get 14899 words
 distinct_words = set([word for text in text_lists for word in text])
+
 # print each text as a bag of words count
 # if we'd want to see which words belong to which column we'd have to list the distinct words as a set is not ordered
 for text in text_lists:
