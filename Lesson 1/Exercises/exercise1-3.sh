6ce67bd11a7b4cda492483d482ae6babcaf792fe
Søren Howe Gersager <syrelyre@gmail.com>
2015-09-08 07:54:29 +0200

    added additional comments to exercise 1-3

--- Lesson 1/Exercises/exercise1-3.sh
+++ Lesson 1/Exercises/exercise1-3.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# we use grep with -E flag and regex "\w+" to get all words and -o flag to get each on a separate output line
+# sort to make duplicate entries consecutive and awk converting all to lowercase
+# uniq to remove consecutive duplicates
+# comm to compare the files, however the files both need to be sorted
+# to sort the dict and make it appear as a file we use process substitution
 if [ $# -eq 2 ]
   then
     grep -E '\w+' -o $1 | sort | awk '{print tolower($0)}' | uniq | comm -23 - <(sort $2)
