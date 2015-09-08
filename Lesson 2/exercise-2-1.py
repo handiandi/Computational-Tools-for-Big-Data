b25f3fe162db7ea3f6f08e1ae8805e87b4d073ca
Søren Howe Gersager <syrelyre@gmail.com>
2015-09-08 16:10:06 +0200

    added input from command line

--- Lesson 2/exercise-2-1.py
+++ Lesson 2/exercise-2-1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 def load_array(path):
 	multi_array = []
@@ -7,12 +8,24 @@ def load_array(path):
 			multi_array.append(line.split())
 	return multi_array
 
-def save_array(multi_array, path="saved_array.txt"):
+def save_array(path, multi_array):
 	with open(path,"w+") as f:
 		for array in multi_array:
 			# could also use a list comprehension
 			f.write(" ".join(map(str, array))+"\n")
 
+if __name__ == "__main__":
+	if len(sys.argv) != 3 or (sys.argv[1] != "load" and sys.argv[1] != "save"):
+		print("exercise-2-1.py OPTION PATH")
+		print("OPTION can be save or load")
+		print("PATH is path to save or load to")
+		sys.exit(1)
 
-print(load_array("array.txt"))
-save_array([[1,2,3],[4,5,6],[7,8,9]])
+	path = sys.argv[2]
+
+	if sys.argv[1] == "load":
+		array = load_array(path)
+		print(array)
+	elif sys.argv[1] == "save":
+		proof_of_concept_array = [[1, 2],[2, 3],[3, 4]]
+		array = save_array(path, proof_of_concept_array)
