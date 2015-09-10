7f8abd06c0063e5249a3af70976c04b1a853fb36
anders <handiiandii@gmail.com>
2015-09-10 16:49:54 +0200

    exercise-2-2 finished! Can take multiple input arg

--- Lesson 2/exercise-2-2.py
+++ Lesson 2/exercise-2-2.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python3
 
 import sys
-#print sys.argv
 from itertools import repeat
 
-def bitStrings(number):
-	bit_String = list(repeat(0, number))
-	bit_String_end = list(repeat(1, number))
-	print(bit_String)
-	print(bit_String_end)
-
-	test = [0,1,2,3]
-	print(test)
-	test.reverse()
-	print(test)
-
-
-	bit_String.reverse()
-	indices_zero = [i for i, x in enumerate(bit_String) if x == 0] #find all indices of 0 in the list bit_String
-	min_index = min(indices_zero)
-	if min_index == 0:
-		bit_String[0] = 1
-
-	else:
-		
-	for index in indices_zero:
-
-		pass
-	#while (bit_String != bit_String_end):
-
-	#	pass
-
-print("Hej python3")
 arguments = sys.argv[1:]
-print(arguments)
 
 for argument in arguments:
 	print("Run binary")
-	bitStrings(int(argument))
+	result = bit_strings(int(argument))
+	print("Bit strings for {0}: \n {1}\n\n".format(argument, result))
 
 
+
+"""
+	Create a list of list of bit strings of the input value
+
+	:param number: The number which all bit strings should be found
+    :return bit_strings: List of all bit strings as lists
+"""
+def bit_strings(number):
+	bit_string = list(repeat(0, number)) #Start bit string, eg. [0, 0, 0]
+	bit_string_end = list(repeat(1, number)) #End bit string, eg. [1, 1, 1]
+
+	bit_strings = []
+	bit_strings.append(bit_string[:])
+	while (bit_string != bit_string_end):
+		bit_string.reverse() #Reverse the order of elements in list
+		indices_zero = [i for i, x in enumerate(bit_string) if x == 0] #find all indices of 0 in the list bit_String
+		min_index = min(indices_zero)
+		if min_index == 0: #There is a zero at index 0
+			bit_string[0] = 1 #Set the zero to one!
+
+		else:
+			for index in range(0,min_index+1):
+				if bit_string[index] == 0:
+					bit_string[index] = 1
+				else:
+					bit_string[index] = 0
+		bit_string.reverse() #Reverse the order of elements in list (back to original order)
+		bit_strings.append(bit_string[:]) #Append all elements by value, not reference! 
+	
+	return bit_strings
