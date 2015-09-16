7ff279bf09a38d2c72f6e797eecdc67ae3cfadc9
anders <handiiandii@gmail.com>
2015-09-16 21:20:54 +0200

    Pandas exercise (2) updated with comments

--- Lesson 3/exercise-3-3.py
+++ Lesson 3/exercise-3-3.py
@@ -3,6 +3,11 @@ from __future__ import print_function #Used to use print function in list compre
 import pandas as pd
 import numpy as np
 
+"""
+Exercise 1
+-----------
+Use the data combining tools discussed above to combine these three objects into a single object named movie_data
+"""
 pd.set_option('display.max_columns', None) #Allow pandas to print unlimmited no. of columns in the terminal
 pd.set_option('display.width', 100) #Allow pandas to print unlimmited no. of characters in the terminal before a new row
 print("Loading data. Please wait...")
@@ -19,17 +24,18 @@ ratings = pd.read_table("ml-1m/ratings.dat", sep="::", names=ratings_col_names) 
 
 movie_data = pd.merge(ratings, users, on="user id") #Merging data from ratings and users on column name 'user id'
 movie_data = pd.merge(movie_data, movies, on="movie id") #Merging the already merged data with movies on column name 'movie id'
-#print(movie_data.iloc[0])
 
 
-#---Exercise 2!
 """
+Exercise 2
+-----------
 The 5 movies with the most number of ratings
 """
 #Gets the top 5 movie title:
 #   (1) Use the value_counts() on dataframe to get the titles and no. of occurences
 #   (2) Use keys() to get only titles (not occurences)
 titles = movie_data['title'].value_counts().keys()
+print("\nThe 5 movies with the most number of ratings:\n------------------------------------")
 [print(title) for title in titles[:5]] #Printing the first 5 titles
 
 """
@@ -38,7 +44,7 @@ A new object called active_titles that is made up of movies each having at least
 occurences =  movie_data['title'].value_counts().values #Get the occurences of the movies (no. of ratings)
 stop_index = np.where(occurences==249)[0][0] #Finding the index where the occurence is 249 (less than 250)
 active_titles = movie_data[movie_data.title.isin(titles[:stop_index])] #Get all data for movies with 250 or more ratings (based of the index from before)
-
+print("\n'active_titles' created\n------------------------------------")
 """ 
 The 3 movies with the highest average rating for females.
 """
