a77a8a0a223e224d75a1fdbecc23a06052a062ed
Soren Howe Gersager <syrelyre@gmail.com>
2015-09-17 19:45:21 +0200

    made scripts executable, added exercise 3-4

--- Lesson 3/exercise3_5_cython.pyx
+++ Lesson 3/exercise3_5_cython.pyx
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import timeit
 
 def cpython_compute_sum(int terms = 10000):
@@ -5,5 +6,5 @@ def cpython_compute_sum(int terms = 10000):
 	for i in range(1, terms+1):
 		sum += 1/(i**2)
 	return sum
-
+# execution time on a i5 2500k = 0.062847165, ~17.6x speedup, not too shabby
 print(timeit.timeit(cpython_compute_sum, number=500))
--- Lesson 3/exercise3_5_python.py
+++ Lesson 3/exercise3_5_python.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import timeit
 
 def python_compute_sum(terms=10000):
@@ -5,5 +6,5 @@ def python_compute_sum(terms=10000):
 	for i in range(1, terms+1):
 		sum += 1/(i**2)
 	return sum
-
+# execution time on a i5 2500k = 1.1080237 
 print(timeit.timeit(python_compute_sum, number=500))
