a54594d08082ab0a3d8472477a638517cdf37fbd
Søren Howe Gersager <syrelyre@gmail.com>
2015-09-24 14:18:24 +0200

    added array for saving distances to reduce computation

--- Lesson 4/dbscan_cython.pyx
+++ Lesson 4/dbscan_cython.pyx
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: cdivision=True
+#cython: nonecheck=False
+#cython: profile=True
+
+import pstats, cProfile
 import pickle
 import os
 import numpy as np
@@ -8,72 +16,92 @@ import timeit
 
 ctypedef np.int_t DTYPE_t
 
-# parameters
-FILENAME = "data_100points_100dims.dat"
 
-EPS = 0.15
-M = 2
 
 def run_program():
+	# parameters
+	FILENAME = "data_100points_100dims.dat"
+	cdef float EPS = 0.3
+	cdef int M = 2
+
 	file_handler = open(os.path.join("data",FILENAME),"rb")
 	cdef np.ndarray[DTYPE_t, ndim=2] X = pickle.load(file_handler, encoding="latin1").todense()
 	file_handler.close()
 
+	cdef np.ndarray[np.float_t, ndim=2] distance_matrix = np.empty((X.shape[0],X.shape[1]), dtype=np.float64)
+	distance_matrix.fill(-1)
+
 	cdef np.ndarray[DTYPE_t, ndim=1] visited_indexes = np.zeros(X.shape[0], dtype=np.int64)
 	cdef np.ndarray[DTYPE_t, ndim=1] cluster_indexes = np.zeros(X.shape[0], dtype=np.int64)
 	
-	db_scan(EPS, M, X, cluster_indexes, visited_indexes)
+	db_scan(EPS, M, X, cluster_indexes, visited_indexes, distance_matrix)
 	print(cluster_indexes)
 	print(np.unique(cluster_indexes).size)
 
-def db_scan(double eps, int m, np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[DTYPE_t, ndim=1] cluster_indexes, np.ndarray[DTYPE_t, ndim=1] visited_indexes):
+def db_scan(float eps, int m, np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[DTYPE_t, ndim=1] cluster_indexes, np.ndarray[DTYPE_t, ndim=1] visited_indexes, np.ndarray[np.float_t, ndim=2] distance_matrix):
 	cdef int p_index, C = 0
 	cdef int num_rows = X.shape[0]
 	for p_index in range(0, num_rows):
 		if visited_indexes[p_index]:
 			continue
 		visited_indexes[p_index] = 1
-		neighbor_points = region_query(p_index, eps, X)
+		neighbor_points = region_query(p_index, eps, X, distance_matrix)
 		if neighbor_points.size < m:
 			cluster_indexes[p_index] = -1
 		else:
 			C += 1
-			expand_cluster(p_index, neighbor_points, C, eps, m, X, cluster_indexes, visited_indexes)
+			expand_cluster(p_index, neighbor_points, C, eps, m, X, cluster_indexes, visited_indexes, distance_matrix)
 
-def expand_cluster(int p_index, np.ndarray[DTYPE_t, ndim=1] neighbor_points, int C, double eps, int m, np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[DTYPE_t, ndim=1] cluster_indexes, np.ndarray[DTYPE_t, ndim=1] visited_indexes):
+def expand_cluster(int p_index, np.ndarray[DTYPE_t, ndim=1] neighbor_points, int C, float eps, int m, np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[DTYPE_t, ndim=1] cluster_indexes, np.ndarray[DTYPE_t, ndim=1] visited_indexes, np.ndarray[np.float_t, ndim=2] distance_matrix):
 	cluster_indexes[p_index] = C
 	cdef int i = 0
 	while i < neighbor_points.size:
 		if not visited_indexes[neighbor_points[i]]:
 			visited_indexes[neighbor_points[i]] = 1
-			neighbor_points_prime = region_query(neighbor_points[i], eps, X)
+			neighbor_points_prime = region_query(neighbor_points[i], eps, X, distance_matrix)
 			if neighbor_points_prime.size >= m:
 				neighbor_points = np.append(neighbor_points, neighbor_points_prime)
 		if cluster_indexes[neighbor_points[i]] == 0:
 			cluster_indexes[neighbor_points[i]] = C
 		i += 1
 
-def compute_jaccard_distance(np.ndarray[DTYPE_t, ndim=2] A, np.ndarray[DTYPE_t, ndim=2] B):
-	M_11 = np.sum(np.multiply(A,B))
-	M_01_and_M_10 = np.sum(np.absolute((A-B)))
-	jaccard_index = (M_11)/ float((M_01_and_M_10 + M_11))
-	jaccard_distance = 1 - jaccard_index
-	return jaccard_distance
+def compute_jaccard_distance(np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[np.float_t, ndim=2] distance_matrix, int a_index, int b_index):
+	cdef int M_11, M_01_and_M_10
+	cdef float jaccard_index, jaccard_distance
 
-def region_query(int p_index, double eps, np.ndarray[DTYPE_t, ndim=2] X):
+	if (distance_matrix[a_index,b_index] != -1):
+		return distance_matrix[a_index,b_index]
+	elif (distance_matrix[b_index, a_index] != -1):
+		return distance_matrix[b_index, a_index]
+	else:
+		M_11 = np.sum(np.multiply(X[a_index,:],X[b_index,:]))
+		M_01_and_M_10 = np.sum(np.absolute((X[a_index,:]-X[b_index,:])))
+		jaccard_index = (M_11)/ float((M_01_and_M_10 + M_11))
+		jaccard_distance = 1 - jaccard_index
+		
+		distance_matrix[a_index, b_index] = jaccard_distance
+		distance_matrix[b_index, a_index] = jaccard_distance
+		return jaccard_distance
+
+def region_query(int p_index, float eps, np.ndarray[DTYPE_t, ndim=2] X, np.ndarray[np.float_t, ndim=2] distance_matrix):
 	""" compute distance for every row other than X(index)
 		if distance < eps, keep neighbour as index
 	"""
 	cdef np.ndarray[DTYPE_t, ndim=1] neighbor_indexes = np.zeros(X.shape[0], dtype=np.int64)
 	cdef int i = 0
+	cdef float distance = 0.0
 	cdef int num_rows = X.shape[0]
 	for i in range(0, num_rows):
-		distance = compute_jaccard_distance(X[i,:], X[p_index,:])
+		distance = compute_jaccard_distance(X, distance_matrix, i, p_index)
 		if distance <= eps:
 			neighbor_indexes[i] = 1
 	# why does it return tuple, fix this
 	return neighbor_indexes.nonzero()[0]
 
+#cProfile.runctx("run_program()", globals(), locals(), "Profile.prof")
+
+#s = pstats.Stats("Profile.prof")
+#s.strip_dirs().sort_stats("time").print_stats()
 
 print(timeit.timeit(run_program, number=1))
 
