0e5863a5ddc185c18463efbcf7254f8670f564f9
Soren Howe Gersager <syrelyre@gmail.com>
2015-09-27 12:43:10 +0200

    moved anders' version to main

--- Lesson 4/dbscan_cython.pyx
+++ Lesson 4/dbscan_cython.pyx
@@ -13,6 +13,7 @@ import os
 import numpy as np
 cimport numpy as np
 import timeit
+import sys
 
 DTYPE = np.int
 DTYPE_FLOAT = np.float
@@ -20,8 +21,8 @@ ctypedef np.int_t DTYPE_t
 ctypedef np.float_t DTYPE_FLOAT_t
 
     # parameters
-FILENAME = "data_100points_100dims.dat"
-cdef float EPS = 0.3
+FILENAME = "data_10000points_10000dims.dat"
+cdef float EPS = 0.15
 cdef int M = 2
 
 file_handler = open(os.path.join("data",FILENAME),"rb")
@@ -51,13 +52,13 @@ def run_program():
 
 def db_scan(float eps, int m):
     cdef int p_index, C = 0
-    cdef int num_rows = distance_matrix.shape[0]
-    for p_index in range(0, num_rows):
-        # check if visited or already in cluster
+    for p_index in range(num_rows):
+        # check if visited
         if visited_indexes[p_index]:
             continue
         visited_indexes[p_index] = 1
         neighbor_points = region_query(p_index, eps)
+        # check if smaller than m-1 since we dont return P
         if neighbor_points.size < m-1:
             cluster_indexes[p_index] = -1
         else:
@@ -73,65 +74,55 @@ def expand_cluster(int p_index, np.ndarray[DTYPE_t, ndim=1] neighbor_points, int
         if not visited_indexes[index]:
             visited_indexes[index] = 1
             neighbor_points_prime = region_query(index, eps)
+            # check if smaller than m-1 since we dont return P
             if neighbor_points_prime.size >= m-1:
                 neighbor_points = np.append(neighbor_points, neighbor_points_prime)
         if cluster_indexes[index] == 0:
             cluster_indexes[index] = C
         i += 1
 
-def compute_jaccard_distance(int a_index, int b_index):
-    cdef int M_11, M_01_and_M_10
+def compute_jaccard_distance(int a_index, int p_index):
+    cdef int M_11 = 0, M_01_and_M_10 = 0
     cdef float jaccard_index, jaccard_distance
 
-    cdef int [:] X_a_index_row = extract_row(a_index)
-    cdef int [:] X_b_index_row = extract_row(b_index)
+    cdef int f_row = indptr_view[a_index] # first column position of value in row 1
+    cdef int s_row = indptr_view[p_index] # first column position of value in row 2
+    cdef int t_row = indptr_view[p_index+1] # first column position of value in row 3
 
-    M_11 = 0
-    M_01_and_M_10 = 0
-    for i in range(0,num_rows):
-        if X_a_index_row[i] == 1 and X_a_index_row[i] == X_b_index_row[i]:
-            M_11 += 1
-        elif X_a_index_row[i] != X_b_index_row[i]:
-            M_01_and_M_10 +=1
+    cdef int f_row_col = indices_view[f_row] # first value of column in row with value
+    cdef int s_row_col = indices_view[s_row] # first column in row with value
+    cdef int t_row_col = indptr_view[p_index+1]
+    cdef int f_row_col_start = indptr_view[a_index]
+    cdef int f_row_col_stop = indptr_view[a_index+1]
+    
+    cdef int s_row_col_start = indptr_view[p_index]
+    cdef int s_row_col_stop = indptr_view[p_index+1]
 
+    for x in range(f_row_col_start, f_row_col_stop):
+        for y in range(s_row_col_start, s_row_col_stop):
+            if indices_view[x] == indices_view[y]:
+                M_11 +=1
+                break
+
+    M_01_and_M_10 = ((f_row_col_stop - f_row_col_start) + (s_row_col_stop - s_row_col_start)) -(2*M_11)
+    if M_01_and_M_10 < 0:
+        M_01_and_M_10 = -M_01_and_M_10
+  
     jaccard_index = (M_11)/ float((M_01_and_M_10 + M_11))
     jaccard_distance = 1-jaccard_index
-    
-    distance_matrix[a_index, b_index] = jaccard_distance
-    distance_matrix[b_index, a_index] = jaccard_distance
+     
+    distance_matrix[a_index, p_index] = jaccard_distance
+    distance_matrix[p_index, a_index] = jaccard_distance
     return jaccard_distance
-
-def extract_row(int i):
-    cdef int[:] row_list = np.empty(num_rows, dtype=np.dtype("i"))
-
-    cdef int current_index = 0
-    cdef int zero_index = 0
-    cdef int j
-
-    for i in range(indptr_view[i], indptr_view[i+1]):
-        j = indices_view[i]
-        while(zero_index < j):
-            row_list[current_index] = 0
-            current_index += 1
-            zero_index += 1
-        row_list[current_index] = 1
-        current_index += 1
-        zero_index += 1
-    while(zero_index < num_rows):
-        row_list[current_index] = 0
-        current_index += 1
-        zero_index += 1
-    return row_list
 
 def region_query(int p_index, float eps):
     """ compute distance for every row other than X(index)
         if distance < eps, keep neighbour as index
     """
-    cdef np.ndarray[DTYPE_t, ndim=1] neighbor_indexes = np.empty(distance_matrix.shape[0], dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=1] neighbor_indexes = np.empty(num_rows, dtype=DTYPE)
     neighbor_indexes.fill(-1)
     cdef int i = 0
     cdef float distance = 0.0
-    cdef int num_rows = distance_matrix.shape[0]
 
     for i in range(0, num_rows):
         if (i == p_index):
@@ -144,7 +135,6 @@ def region_query(int p_index, float eps):
             distance = compute_jaccard_distance(i, p_index)
         if distance <= eps:
             neighbor_indexes[i] = 1
-    # why does it return tuple, fix this
     return np.where(neighbor_indexes != -1)[0]
 
 cProfile.runctx("run_program()", globals(), locals(), "Profile.prof")
@@ -153,10 +143,10 @@ s = pstats.Stats("Profile.prof")
 s.strip_dirs().sort_stats("tottime").print_stats()
 
 #print(timeit.timeit(run_program, number=1))
-
-assert (np.array([ 1,  2, -1,  3,  4, -1,  4,  4, -1,  4, -1,  2,  2,  1, -1,  1,  1,  4,
- -1,  1,  2,  1,  1,  4, -1, -1,  2,  2,  1,  4,  4,  4,  4,  1, -1, -1,
-  2,  1, -1,  4,  1, -1,  2,  1,  1,  4,  4,  4,  4,  1,  2,  4,  2,  4,
- -1,  1,  4, -1,  3, -1, -1,  1, -1, -1,  4, -1,  2,  2,  3,  5,  1, -1,
-  1,  4, -1,  2,  2,  4, -1, -1,  1,  4, -1, -1,  5,  2,  2,  1, -1,  1,
- -1,  1, -1,  1, -1,  2,  1, -1,  2,  2,]) == cluster_indexes).all()
+if (FILENAME == "data_100points_100dims.dat"):
+    assert (np.array([ 1,  2, -1,  3,  4, -1,  4,  4, -1,  4, -1,  2,  2,  1, -1,  1,  1,  4,
+     -1,  1,  2,  1,  1,  4, -1, -1,  2,  2,  1,  4,  4,  4,  4,  1, -1, -1,
+      2,  1, -1,  4,  1, -1,  2,  1,  1,  4,  4,  4,  4,  1,  2,  4,  2,  4,
+    -1,  1,  4, -1,  3, -1, -1,  1, -1, -1,  4, -1,  2,  2,  3,  5,  1, -1,
+      1,  4, -1,  2,  2,  4, -1, -1,  1,  4, -1, -1,  5,  2,  2,  1, -1,  1,
+     -1,  1, -1,  1, -1,  2,  1, -1,  2,  2,]) == cluster_indexes).all()
--- Lesson 4/run_dbscan_cython.py
+++ Lesson 4/run_dbscan_cython.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pyximport
 pyximport.install()
-import dbscan_cython
+import dbscan_cython_anders
 
 #tottime = 0.631, cumtime = 2.045 #uden xor
 #tottime = 0.527, cumtime = 2.114 #med xor
