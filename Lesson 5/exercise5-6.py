2eb4d96058ddfa15a582c4aa80c3c0bf50b23220
Søren Howe Gersager <syrelyre@gmail.com>
2015-10-06 16:12:05 +0200

    exercise 5-6 added

---
+++ Lesson 5/exercise5-6.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sqlite3
+import pymongo
+"""
+Exercise 5.5
+Sqlite version of exercise:
+We first find the distinct customer ids of the people who've purchased Pears using the DISTINCT and procede just like in the former exercise.
+Next we find all the orders orders of the customers by joining with orders, then order details joining with order details and products joining with products.
+We then find the distinct products by joining the beforementioned tables again and
+build a string for use with the IN keyword specifying all the customer id's whose products we are interested in.
+"""
+con = sqlite3.connect("northwind.db")
+con.text_factory = lambda x: str(x, 'latin1')
+cur = con.cursor()
+cur.execute("""SELECT DISTINCT Orders.CustomerID from Orders
+			   INNER JOIN Customers on Orders.CustomerID = Customers.CustomerID 
+			   INNER JOIN 'Order Details' on Orders.OrderID = 'Order Details'.OrderID 
+			   INNER JOIN Products on 'Order Details'.ProductID = Products.ProductID  
+			   WHERE Products.ProductID = 7 GROUP BY Orders.CustomerID""")
+# produce a list of values instead of a list of single-tuples
+customer_ids = [customer_id[0] for customer_id in cur.fetchall()]
+placeholder = "?"
+placeholders = ",".join(placeholder*len(customer_ids))
+
+cur.execute(""" SELECT count(Products.ProductName), Products.ProductName from Products
+				INNER JOIN 'Order Details' on Products.ProductID = 'Order Details'.ProductID
+				INNER JOIN Orders on 'Order Details'.OrderID = Orders.OrderID
+				INNER JOIN Customers on Orders.CustomerID = Customers.CustomerID WHERE Customers.CustomerID IN (%s) GROUP BY Products.ProductName""" % placeholders, customer_ids)
+products = cur.fetchall()
+most_purchased_product = max(products, key=lambda v: v[0])
+
+print("Sqlite: {} is most purchased with {} purchases".format(most_purchased_product[1],most_purchased_product[0]))
+
+con.close()
+
+""" MongoDB version of exercise:
+We first find the customers who purchased pears like in the last exercise
+Next we find all the Orders and Order Details and their corresponding Products
+We can then convert the list to a set to find the unique products and how many there is
+"""
+client = pymongo.MongoClient('localhost', 27017)
+db = client["Northwind"]
+customer_collection = db["customers"]
+order_collection = db["orders"]
+order_details_collection = db["order-details"]
+products_collection = db["products"]
+
+customer_ids_list = []
+pear_details = order_details_collection.find({"ProductID":7})
+for detail in pear_details:
+	for order in order_collection.find({"OrderID": detail["OrderID"]}):
+		customer_ids_list.append(order["CustomerID"])
+
+products = []
+for customer in set(customer_ids_list):
+	for order in order_collection.find({"CustomerID":customer}):
+		for detail in order_details_collection.find({"OrderID":order["OrderID"]}):
+			products.append(products_collection.find_one({"ProductID":detail["ProductID"]})["ProductName"])
+
+most_purchased = max(products, key=lambda v: products.count(v))
+print("MongoDB: {} is most purchased with {} purchases".format(most_purchased, products.count(most_purchased)))
+
