a0ed08ee2509016c00228c534d1d978a5288dc74
Søren Howe Gersager <syrelyre@gmail.com>
2015-12-02 21:53:56 +0100

    changed exercise to create RDD

--- Assignment 4/exercise-9-1.py
+++ Assignment 4/exercise-9-1.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from pyspark import SparkContext
+from pyspark import SparkContext,SparkFiles
 
 # apache spark test job
 
 # Write a Spark job to count the occurrences of each word in a text file. Document that it works with a small example.
 CONNECTION_STR = "spark://"+os.environ["SPARK_MASTER_PORT_7077_TCP_ADDR"]+":"+os.environ["SPARK_MASTER_ENV_SPARK_MASTER_PORT"]
 sc = SparkContext(CONNECTION_STR,"test")
-
-lines = sc.textFile("exercise1_data.txt") #Extracting the lines in the file
+sc.addFile(os.path.join(os.path.dirname(os.path.realpath(__file__)),"exercise1_data.txt"))
+with open(SparkFiles.get("exercise1_data.txt")) as testFile:
+    lines = sc.parallelize(testFile.readlines())
 
 words = lines.flatMap(lambda s: s.split()) #Splitting the lines into words
 pairs = words.map(lambda s: (s, 1)) #Creating a pair for each word in the form (word, count) where count is the occurrence of the word, set to 1
