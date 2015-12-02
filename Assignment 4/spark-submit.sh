9c7a134217161d244e1ac78319f0d22841d43aca
Søren Howe Gersager <syrelyre@gmail.com>
2015-12-02 20:26:51 +0100

    added spark-submit which does cp

--- Assignment 4/spark-submit.sh
+++ Assignment 4/spark-submit.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-docker run -i -t -P --link spark_master:spark_master test /spark-submit.sh "$@"
+
+CONTAINER_ID="$(docker run -i -t -P -d --link spark_master:spark_master test)"
+echo "CONTAINER ID:"
+echo "${CONTAINER_ID}"
+echo "trying to CP"
+docker cp "$@" "${CONTAINER_ID}:/usr/local/spark-1.3.0-bin-hadoop2.4/"
+echo "trying to exec"
+docker exec "${CONTAINER_ID}" "/spark-submit.sh" "$@"
+echo "trying to stop and rm"
+docker stop "${CONTAINER_ID}"
+docker rm "${CONTAINER_ID}"
